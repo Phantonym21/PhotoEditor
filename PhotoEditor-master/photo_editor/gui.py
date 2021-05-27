08bfba6fedeaf4b3bb60d039b78d83749b01b5a3
Phantonym21 <71942501+Phantonym21@users.noreply.github.com>
2021-05-27 16:08:51 +0530

    updated code as per python version 3.9

---
+++ PhotoEditor-master/photo_editor/gui.py
@@ -0,0 +1,138 @@
+import os
+from tkinter import *
+from  tkinter.filedialog import *
+import tkinter.messagebox as tkMb
+import PIL.Image
+import PIL.ImageTk
+
+from photo_editor.image import Image
+from photo_editor import filters
+
+
+class GUI(object):
+    """
+    GUI Class
+    """
+    def __init__(self):
+        """
+        GUI Constructor
+        """
+        self.master = Tk()
+
+        self.master.wm_title('Photo Editor')
+
+        self.img = None
+        self.pil_image = None
+        self.photo = None
+        self.showing = None
+
+        self.master.geometry('{}x{}'.format(850, 500))
+
+        # Adding in the file menu
+        self.menubar = Menu(self.master)
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label='Open Image', command=self.select_picture)
+        self.filemenu.add_command(label='Save Image', command=self.save_image)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label='Exit') # Add Command
+        self.menubar.add_cascade(label='File', menu=self.filemenu)
+
+        # Adding edit menu
+        self.editmenu = Menu(self.menubar, tearoff=0)
+        self.editmenu.add_command(label='Undo', command=self.undo)
+        self.editmenu.add_command(label='Reset', command=self.reset)
+        self.menubar.add_cascade(label='Edit', menu=self.editmenu)
+
+        self.master.config(menu=self.menubar)
+        # ------
+
+        self.actions_pane = PanedWindow(self.master)
+        self.actions_pane.pack(fill=BOTH, expand=1)
+
+        # init filter select
+        self.filter_select_pane = PanedWindow(self.actions_pane, orient=VERTICAL)
+        self.actions_pane.add(self.filter_select_pane)
+
+
+        self.var = StringVar(self.master)
+        self.var.trace_variable('w', self.select_filter)
+
+        opts_frame = Frame(self.filter_select_pane)
+        opts_frame.pack()
+
+        self.filter_select_pane.add(opts_frame)
+
+        self.filter_params_pane = PanedWindow(self.filter_select_pane)
+        self.filter_select_pane.add(self.filter_params_pane)
+
+        self.opts_vals = {
+            'Gaussian Filter': filters.GaussianFilter(self.filter_params_pane, self.apply_filter),
+            'Box Filter': filters.BoxFilter(self.filter_params_pane, self.apply_filter),
+            'Median Filter': filters.MedianFilter(self.filter_params_pane, self.apply_filter),
+            'Canny Edge Detector': filters.CannyEdgeDetector(self.filter_params_pane, self.apply_filter),
+            'Bilateral Filter': filters.BilateralFilter(self.filter_params_pane, self.apply_filter),
+            'Non-Local Means Filter': filters.NonLocalMeansColor(self.filter_params_pane, self.apply_filter),
+            'Sobel Filter': filters.SobelFilter(self.filter_params_pane, self.apply_filter),
+            'Laplacian Filter': filters.LaplacianFilter(self.filter_params_pane, self.apply_filter)}
+
+        self.opts = OptionMenu(opts_frame, self.var, *self.opts_vals.keys())
+        self.opts.pack()
+
+        self.var.set(list(self.opts_vals.keys())[0])
+
+        self.content_pane = PanedWindow(self.actions_pane, orient=VERTICAL)
+        self.actions_pane.add(self.content_pane)
+
+        self.cont_lbl = Label(self.content_pane, text='No image selected')# image=self.photo)
+        self.cont_lbl.place(x=0, y=0, width=750, height=750)
+        self.cont_lbl.grid()
+        self.content_pane.add(self.cont_lbl)
+
+    def main(self):
+        self.master.mainloop()
+
+    def select_filter(self, *args):
+        if self.showing is not None:
+            self.opts_vals[self.showing].tk_hide()
+
+        self.showing = self.var.get()
+        self.reset()
+        self.opts_vals[self.var.get()].tk_show()
+
+    def select_picture(self):
+        filename = askopenfilename(parent=self.master)
+
+        if filename != '':
+            if os.path.exists(filename):
+                self.img = Image(filename)
+                self.update_picture()
+            else:
+                tkMb.showinfo('ERROR', '"{0}" does not exist'.format(filename))
+
+    def update_picture(self):
+        self.pil_image = PIL.Image.fromarray(self.img.image)
+        self.photo = PIL.ImageTk.PhotoImage(image=self.pil_image)
+        self.cont_lbl.configure(text='', image=self.photo)
+
+    def save_image(self):
+        outfilename = asksaveasfilename(parent=self.master)
+        self.img.save(outfilename)
+
+    def apply_filter(self, img_filter):
+        if self.img is not None:
+            self.img.apply_filter(img_filter)
+            self.update_picture()
+
+    def undo(self):
+        if self.img is not None:
+            self.img.undo()
+            self.update_picture()
+
+    def reset(self):
+        if self.img is not None:
+            self.img.reset()
+            self.update_picture()
+
+
+    if __name__ == '__main__':
+        main()
---
+++ PhotoEditor-master/photo_editor/image.py
@@ -0,0 +1,102 @@
+"""
+Image Module
+"""
+import cv2
+import os
+from .exceptions import *
+
+
+class Image(object):
+    """
+    Image Class
+    """
+    def __init__(self, filename):
+        """
+        Image class constructor
+
+        Throws an ImageNotExist exception if the given file does not exist.
+
+        Parameters:
+            filename - Filename of the image to load
+        """
+        if not os.path.exists(filename):
+            raise ImageNotExist('Image "{0}" does not exist'.format(filename))
+
+        self.filename = filename
+        self._img = cv2.imread(filename)
+        self._orig_img = self._img
+        self._prev_img = self._img
+        self.cvt_flag = cv2.COLOR_BGR2RGBA
+        self.show_normal = False
+        self.prev_show_normal = False
+
+    @property
+    def origonal(self):
+        """
+        The origonal image that was read in
+        """
+        return self._orig_img
+
+    @property
+    def image(self):
+        """
+        The current image
+        """
+        if self.show_normal:
+            return self._img
+        else:
+            return cv2.cvtColor(self._img, self.cvt_flag)
+
+    def reset(self):
+        """
+        Reset back to the origonal image
+        """
+        self._img = self.origonal
+        self.show_normal = False
+
+    def undo(self):
+        """
+        Undo the last filter
+        """
+        self._img = self._prev_img
+        self.show_normal = self.prev_show_normal
+
+    def save(self, filename, overwrite=False):
+        """
+        Save the image with all filters
+
+        If overwrite is set to False, then an ImageAlreadyExists exception
+        will be thrown if the given filename already exists.
+
+        Parameters:
+            filename  - Name of the file to save the image as
+            overwrite - (Optional) Flag for whether or not to overwrite the
+                        file if it already exists.  By default this is set to
+                        NOT overwrite the file.
+        """
+        if os.path.exists(filename) and not overwrite:
+            raise ImageAlreadyExists(
+                'Image "{0}" already exists'.format(filename))
+        cv2.imwrite(filename, self._img)
+
+    def apply_filter(self, img_filter):
+        """
+        Apply the given filter to the image.
+
+        Parameters:
+            img_filter - A filter of type BaseFilter
+        """
+        self._prev_img = self._img
+        self.prev_show_normal = self.show_normal
+
+        try:
+            self.show_normal = img_filter.show_normal
+            self._img = img_filter.filter(self._orig_img)
+
+            if img_filter.set_to_origonal:
+                self.reset()
+        except AssertionError:
+            if img_filter.set_to_origonal:
+                self.reset()
+            else:
+                self.undo()
